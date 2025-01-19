5e972b00d976530529cd90bb678741122f2190f1
Dhanushmathan <lovelymathan2394@gmail.com>
2025-01-19 18:38:47 +0530

    modify: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SignupPage from './components/pages/SignupPage';
-import { Route, Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <SignupPage />
+    <div>
+      <SignupPage />
+    </div>
   )
 }
 
