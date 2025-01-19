15607c9185a9ecde83cc1185194283062627eb12
Dhanushmathan <lovelymathan2394@gmail.com>
2025-01-19 18:13:51 +0530

    fix: run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import SignupPage from './components/pages/SignupPage';
+import { Route, Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <SignupPage />
-    </div>
+    <SignupPage />
   )
 }
 
